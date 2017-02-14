b56917cea639a7b07f612236ae503f9c27448112
Abdul Hameed <raza2022@gmail.com>
2017-02-14 15:10:05 +0500

    crop image after detect

--- demo2/detectFaces.js
+++ demo2/detectFaces.js
@@ -9,6 +9,7 @@ function detectImages(e){
         complete: function (faces) {
             console.log(faces.length);
             for (var i = 0; i < faces.length; i++) {
+                cropImages(faces[i]);
                 $('<div>', {
                     'class':'face',
                     'css': {
@@ -31,4 +32,16 @@ function detectImages(e){
 //convert the image to base 64
 function base64Image(){
     $("#picture").attr("src", $('#detectedImage')[0].toDataURL());
+}
+
+function cropImages (f){
+    if(f){
+        var i = document.getElementById("detectedImage");
+        var c = document.getElementById("cropImage");
+        var ctx = i.getContext("2d");
+        var ctx1 = c.getContext("2d");
+        var croppedImage = ctx.getImageData(f.x, f.y, f.width, f.height);
+        ctx1.putImageData(croppedImage, 0, 0);
+    }
+
 }
