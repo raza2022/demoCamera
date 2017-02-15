8177416c06cbaea63a279f5d20a862fad624f919
Abdul Hameed <raza2022@gmail.com>
2017-02-15 12:02:18 +0500

    multiple image displayed

--- demo2/detectFaces.js
+++ demo2/detectFaces.js
@@ -1,3 +1,4 @@
+var canvasObjects = {};
 //detect the face from base64 image
 function detectImages(e){
     e.preventDefault();
@@ -7,9 +8,8 @@ function detectImages(e){
     //$('#base64').faceDetection({
     $('#picture').faceDetection({
         complete: function (faces) {
-            console.log(faces.length);
             for (var i = 0; i < faces.length; i++) {
-                cropImages(faces[i]);
+                cropImages(faces[i], i);
                 $('<div>', {
                     'class':'face',
                     'css': {
@@ -34,14 +34,26 @@ function base64Image(){
     $("#picture").attr("src", $('#detectedImage')[0].toDataURL());
 }
 
-function cropImages (f){
-    if(f){
-        var i = document.getElementById("detectedImage");
-        var c = document.getElementById("cropImage");
-        var ctx = i.getContext("2d");
-        var ctx1 = c.getContext("2d");
-        var croppedImage = ctx.getImageData(f.x, f.y, f.width, f.height);
-        ctx1.putImageData(croppedImage, 0, 0);
-    }
+function cropImages (f, index){
+    //face found
+    if( f ){
+        //get body
+        var body = $('body')[0];
+        //create new canvas for cropped Image
+        canvasObjects[index] = document.createElement('canvas');
 
+        //get old canvas for cropping
+        var i = document.getElementById("detectedImage");
+        //get old image context
+        var ctx = i.getContext("2d");
+        //get new created canvas context
+        var ctx1 = canvasObjects[index].getContext("2d");
+
+        //now get to x y and width and height of current Face
+        var croppedImage = ctx.getImageData(f.x, f.y, f.width, f.height);
+        //draw image with given values to new canvas
+        ctx1.putImageData(croppedImage, 0, 0);
+        //all done just append the canvas to body
+        body.appendChild(canvasObjects[index])
+    }
 }
